1252229b8288c672e63a1bb8e95a4ea69143a437
Muiz Yusuff <muiz.rpi@gmail.com>
2020-07-02 08:20:30 -0400

    Grabbed stored user info from the local storage

--- orderCompleteApp.js
+++ orderCompleteApp.js
@@ -1,9 +1,11 @@
 window.addEventListener("load", function () {
   let reciept_id = localStorage.getItem("donation_reciept");
+  let user_data = JSON.parse(localStorage.getItem("donator_info"));
+
   localStorage.removeItem("donation_reciept");
   if (reciept_id) {
     chrome.runtime.sendMessage(
-      { reciept_id, type: "process_donation" },
+      { reciept_id, user_data, type: "process_donation" },
       function (response) {
         console.log(`Message Response:`);
         console.log(response);
