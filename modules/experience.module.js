3a58574a821b5fab236e2d9121b90ec622034dd8
Muiz Yusuff <muiz.rpi@gmail.com>
2020-07-05 13:52:58 -0400

    Changed experience function to take ln base 10

--- modules/experience.module.js
+++ modules/experience.module.js
@@ -1,14 +1,13 @@
 // Calculate the level of a user based on the experience
 
 const evaluateExperience = (experience_value) => {
-    if (experience_value == 0) return 1
-    return Math.log((5 * experience_value) + 1)
-    return experience_value + 1
-}
+  if (experience_value == 0) return 1;
+  return Math.log10(5 * experience_value + 1);
+  return experience_value + 1;
+};
 
 const getLevel = (experience_value) => {
-    
-    return Math.floor(evaluateExperience( experience_value )).toFixed(0)
-}
+  return Math.floor(evaluateExperience(experience_value)).toFixed(0);
+};
 
-export { evaluateExperience, getLevel }
+export { evaluateExperience, getLevel };
